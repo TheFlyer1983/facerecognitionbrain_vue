0dedac6351105ff337d3f1554af7e263132159cc
Paul Livingstone <36920884+TheFlyer1983@users.noreply.github.com>
2021-12-17 18:09:18 +0000

    Added tsparticles background (#15)

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import App from './App.vue';
 import store from './store'
 import router from './router';
 
+import Particles from 'particles.vue3';
+
 createApp(App)
+  .use(Particles)
   .use(store)
   .use(router)
   .mount('#app');
