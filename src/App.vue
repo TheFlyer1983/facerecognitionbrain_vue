a9e530444a1019cc37db5cd128742345b8ab3134
TheFlyer1983 <theflyer1983@gmail.com>
2021-12-17 18:09:00 +0000

    Added tsparticles background

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import particlesObject from '@/assets/particles/particles.json';
 </script>
 
 <template>
+  <Particles id="tsparticles" class="particles" :options="particlesObject" />
   <img alt="Vue logo" src="./assets/logo.png" />
   <router-view />
 </template>
@@ -16,5 +18,16 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  
+}
+
+.particles{
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: -1;
+  background: linear-gradient(89deg, #FF5EDF 0%, #04C8DE 100%);
 }
 </style>
