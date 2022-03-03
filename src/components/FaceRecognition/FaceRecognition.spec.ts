d84787aa97e8021b2299373cc075518870bf0cf3
TheFlyer1983 <theflyer1983@gmail.com>
2022-03-03 17:02:11 +0000

    All Views and Functions tested

--- src/components/FaceRecognition/FaceRecognition.spec.ts
+++ src/components/FaceRecognition/FaceRecognition.spec.ts
@@ -6,7 +6,7 @@ import { useStore } from 'vuex';
 import FaceRecognition from './FaceRecognition.vue';
 import { ImageState } from '@/store/modules/image/imageTypes';
 import { boxesMock } from '@/fixtures/images';
-import { calculateFaceLocations } from '@/functions/imageFunctions'
+import { calculateFaceLocations } from '@/functions/imageFunctions';
 
 vi.mock('vuex');
 vi.mock('@/functions/imageFunctions');
@@ -17,17 +17,18 @@ const mockedCalculateFaceLocations = vi.mocked(calculateFaceLocations, true);
 const mockedImageURL =
   'https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGZhY2V8ZW58MHx8MHx8&w=1000&q=80';
 
-const mockedFaceLocation = [{
-  leftCol: 105.42601499999999,
-  topRow: 74.074805,
-  rightCol: 122.83062999999999,
-  bottomRow: 44.97919999999999 
-}]
+const mockedFaceLocation = [
+  {
+    leftCol: 105.42601499999999,
+    topRow: 74.074805,
+    rightCol: 122.83062999999999,
+    bottomRow: 44.97919999999999
+  }
+];
 
 describe('Given the `FaceRecognition` component', () => {
   const render = () => shallowMount(FaceRecognition);
   let wrapper: VueWrapper<InstanceType<typeof FaceRecognition>>;
-
 
   const mockedGetters = {
     ['image/getImageURL']: mockedImageURL as ImageState['imageUrl'],
@@ -35,7 +36,7 @@ describe('Given the `FaceRecognition` component', () => {
   };
 
   const mockedStore = reactive({
-    getters: mockedGetters,
+    getters: mockedGetters
   });
 
   beforeAll(() => {
