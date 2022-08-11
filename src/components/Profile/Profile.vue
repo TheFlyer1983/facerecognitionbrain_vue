33eef4eaacae841f4f1a5149c9d93d035b238050
theflyer1983 <theflyer1983@gmail.com>
2022-08-11 17:31:11 +0100

    Update date format

--- src/components/Profile/Profile.vue
+++ src/components/Profile/Profile.vue
@@ -70,7 +70,7 @@ async function deleteUser() {
           <h1>{{ user?.name }}</h1>
           <h4>Images Submitted: {{ user?.entries }}</h4>
           <p>
-            Member Since: {{ new Date(`${user?.joined}`).toLocaleDateString() }}
+            Member Since: {{ new Date(`${user?.joined}`).toLocaleDateString('en-GB') }}
           </p>
           <hr />
           <template v-if="showConfirmation">
