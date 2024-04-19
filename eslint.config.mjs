cbe5570d864191d3d7e6d2beff000d093aee30d9
theflyer1983 <theflyer1983@gmail.com>
2024-04-19 17:44:43 +0100

    Initial Setup Nuxt / ESLint / Prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs';
+
+export default withNuxt(
+  // Your custom configs here
+  {
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    }
+  }
+);
