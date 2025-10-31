a46045e9588e65ce6b042b67b61c3629087f4f44
theflyer1983 <theflyer1983@gmail.com>
2025-10-31 14:49:26 +0000

    Remove console.log

--- server/api/rank/rank.get.ts
+++ server/api/rank/rank.get.ts
@@ -2,7 +2,7 @@ const emojis = ['😄', '😃', '😀', '😊', '😉', '😍', '🔸', '🔹', 
 Error;
 export default defineEventHandler(async (event) => {
   const rank = Number(getQuery(event).rank);
-  console.log('Rank requested:', rank);
+
   if (!isNaN(rank)) {
     const rankEmoji = emojis[rank >= emojis.length ? emojis.length - 1 : rank];
 
