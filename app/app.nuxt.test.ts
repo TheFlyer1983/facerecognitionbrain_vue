4fa2b1937139c394309bc3799e8b964c0135cafb
theflyer1983 <theflyer1983@gmail.com>
2026-03-20 19:58:50 +0000

    Refactor App.vue tests and update snapshots
    - Changed the test structure for App.vue to use beforeEach for component mounting.
    - Updated the snapshot test to focus on the shell element instead of the entire component.
    - Adjusted the snapshot file to reflect changes in the App.vue structure and styling.

--- app/app.nuxt.test.ts
+++ app/app.nuxt.test.ts
@@ -1,15 +1,21 @@
-import { describe, it, expect } from 'vitest';
 import App from './app.vue';
 import { mountSuspended } from '@nuxt/test-utils/runtime';
 
-describe('App.vue', async () => {
-  const component = await mountSuspended(App, { route: '/login' });
+describe('App.vue', () => {
+  let component: Awaited<ReturnType<typeof mountSuspended>>;
+
+  beforeEach(async () => {
+    component = await mountSuspended(App, { route: '/login' });
+  }); // = await mountSuspended(App, { route: '/login' });
 
   it('mounts the app component', () => {
     expect(component.exists()).toBe(true);
   });
 
   it('matches the snapshot', () => {
-    expect(component.element).toMatchSnapshot();
+    // expect(component.element).toMatchSnapshot();
+    const shell = component.find('.particles.py-3');
+    expect(shell.exists()).toBe(true);
+    expect(shell.element).toMatchSnapshot();
   });
 });
